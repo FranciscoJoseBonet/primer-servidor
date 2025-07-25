3ece91fd20fb085cbc8dc2d35824c4360d243d6c
FranciscoJoseBonet Desktop <franciscojosebonet@gmail.com>
2025-07-25 11:00:54 -0300

    Update package.json and app.js for servidor-express setup

--- servidor-express/src/app.js
+++ servidor-express/src/app.js
@@ -1,3 +1,14 @@
 import express from "express";
 
 const app = express();
+
+const PORT = 8080;
+
+app.get("/saludo", (req, res) => {
+	res.send("Servidor online");
+});
+
+// sumamente importante implementar el listen sino el servidor esta inerte
+app.listen(PORT, () => {
+	console.log(`Escuchando el puerto ${PORT}`);
+});
